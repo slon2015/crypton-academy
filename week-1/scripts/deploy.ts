ed28e63d6f8cc9d3d796a6ffad32c4ae98b836ff
Serafim <serafim.gerasimov@mail.ru>
2022-03-15 21:17:29 +0300

    Add verification step

--- week-1/scripts/deploy.ts
+++ week-1/scripts/deploy.ts
@@ -1,22 +1,34 @@
-import { ethers } from "hardhat";
+import * as hre from "hardhat";
 
 async function main(): Promise<void> {
-    const [deployer] = await ethers.getSigners();
-
-    console.log("Deploying contracts with the account:", deployer.address);
-
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy(
+    const [deployer] = await hre.ethers.getSigners();
+    const args: [string, string, number, string, number] = [
         "CryptoRuble",
         "cRUB",
         2,
         deployer.address,
-        100000000
-    );
+        100000000,
+    ];
 
-    await token.deployed();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const Token = await hre.ethers.getContractFactory("Token");
+    const token = await Token.deploy(...args);
+
+    if (hre.network.name === hre.config.defaultNetwork) {
+        await token.deployed();
+    } else {
+        await token.deployTransaction.wait(5);
+    }
 
     console.log("Token address:", token.address);
+
+    if (hre.network.name !== hre.config.defaultNetwork) {
+        await hre.run("verify:verify", {
+            address: token.address,
+            constructorArguments: args,
+        });
+    }
 }
 
 main().catch((error) => {
